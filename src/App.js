40c6311c3a90007fb6c15e18e169dc1f4fb6f098
lhwei456 <lhwei.winni@gmail.com>
2022-08-22 15:02:31 +0800

    React Router 動態路由

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {
   NavLink,
   Route,
   Routes,
-  useNavigate
+  useNavigate,
+  useParams,
+  Outlet
 } from 'react-router-dom';
 
 const isLogin = false;
@@ -32,7 +34,19 @@ const Logout = () => {
     }})
   }
   return <button onClick={()=> {navigate('/login', { state: { id: 1}})}}>Logout</button>
-}
+};
+const Post = () => {
+  return (
+    <div>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </div>
+  )
+};
+const PostId = () => {
+  let params = useParams();
+  return <p>PostID: {params.postId}</p>;
+};
 
 function App() {
   return (
@@ -51,6 +65,12 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/123">
+            <p>Post 內頁</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
@@ -58,6 +78,9 @@ function App() {
           <Route path="/todo" element={<Todo />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
           <Route
             path="*"
             element={
